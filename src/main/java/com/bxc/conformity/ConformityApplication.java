6de1e58ca13982ee0898957aaaf9dea694831cc2
app94 <1605834520@qq.com>
2020-07-15 08:41:50 +0800

    a

---
+++ src/main/java/com/bxc/conformity/ConformityApplication.java
@@ -0,0 +1,13 @@
+package com.bxc.conformity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConformityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConformityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bxc/conformity/ConformityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bxc.conformity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConformityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
